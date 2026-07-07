436d1f3b37d94388fe74d8e04efb0473bfb05b9b
payalpawar2006 <pawarpayal2230@gmail.com>
2026-07-07 08:06:00 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[]args)
+    {
+       Console.WriteLine("Payal Pawar");
+Console.WriteLine("Branch- Information Technology"); 
+Console.WriteLine("College- SSGMCE, Shegaon "); 
+
+First f = new First();
+f.display();
+    }
+}
+
